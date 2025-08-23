3f4f7c63b300eef93457f61d4ea2f2afbb69288d
StellAsh45 <mlucioguillen@gmail.com>
2025-08-23 16:19:59 -0500

    Interfaz de la biblioteca

---
+++ Proyecto/Biblioteca.py
@@ -0,0 +1,12 @@
+# Módulo principal de la Biblioteca Virtual Personal
+import tkinter as tk
+
+def iniciar_gui(usuario):
+    root = tk.Tk()
+    root.title("Biblioteca Virtual Personal")
+    root.geometry("800x900")
+
+    label = tk.Label(root, text=f"Bienvenido {usuario} a tu Biblioteca Virtual Personal")
+    label.pack()
+
+    root.mainloop() 
